4feacb5e26613dbd65f7829ee4610490dd3952ac
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-02-20 15:30:12 -0500

    Connected to database

--- Course Project - Phase One/process.php
+++ Course Project - Phase One/process.php
@@ -4,3 +4,14 @@ $host = 'localhost';
 $db   = 'time_tracker';
 $user = 'root';
 $password = '';
+
+// Connect to the database
+$dsn = "mysql:host=$host;dbname=$db";
+
+try {
+   $pdo = new PDO ($dsn, $user, $password); 
+   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Database connection failed: " . $e->getMessage()); 
+}
+?>
