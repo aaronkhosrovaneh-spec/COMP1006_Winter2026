ce7cfe9a70bbb25d4f972c9aaab58a5e1bf1f7c1
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-02-20 17:47:01 -0500

    Created prepare and execute statements

--- Course Project - Phase One/delete.php
+++ Course Project - Phase One/delete.php
@@ -3,4 +3,8 @@ require 'process.php';
 
 //Check if ID was provided
 if (isset($_GET['id'])) {
+    // Prepare statement
+    $stmt = $pdo->prepare("DELETE FROM tasks WHERE id = ?");
+    //Execute statement
+    $stmt->execute([$_GET['id']]);
 }
