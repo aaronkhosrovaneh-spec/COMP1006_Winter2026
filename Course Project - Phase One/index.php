c4c8ed3f83272e9b8bc0bdd84e4842bac8232f33
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-02-20 15:31:50 -0500

    Added SQL query

--- Course Project - Phase One/index.php
+++ Course Project - Phase One/index.php
@@ -9,3 +9,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_task'])) {
     $priority = $_POST['priority'];
     $due_date = $_POST['due_date'] ?? null;
     $time = filter_input(INPUT_POST, 'time', FILTER_VALIDATE_INT);
+
+    // SQL query using named placeholders
+    $sql = "INSERT INTO tasks (name, category, priority, due_date, time)
+            VALUES (:name, :category, :priority, :due_date, :time)";
