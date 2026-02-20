49e1cc6ad572651f98c5de90eac9ff27a58decbd
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-02-20 15:31:29 -0500

    Sanitized text inputs

--- Course Project - Phase One/index.php
+++ Course Project - Phase One/index.php
@@ -3,3 +3,9 @@ require 'process.php';
 
 // Process form
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['add_task'])) {
+    // Sanitize text inputs
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_SPECIAL_CHARS);
+    $priority = $_POST['priority'];
+    $due_date = $_POST['due_date'] ?? null;
+    $time = filter_input(INPUT_POST, 'time', FILTER_VALIDATE_INT);
