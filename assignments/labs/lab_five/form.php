53de3a6bc28d014bf2cc221132994831b10782a4
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-03-22 18:16:15 -0400

    Create form.php

---
+++ assignments/labs/lab_five/form.php
@@ -0,0 +1,5 @@
+<form action="upload-processing.php" method="POST" enctype="multipart/form-data">
+    <label for="profile_pic">Select Profile Picture:</label>
+    <input type="file" name="photo" id="profile_pic">
+    <button type="submit">Upload Image</button>
+</form>
