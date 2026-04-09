c41b3ae909d3b2ae8009bdb84dfc4b9bb7f100a0
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-09 14:26:17 -0400

    Sanitized username input to prevent XSS attacks

--- Course Project - Phase Two/register.php
+++ Course Project - Phase Two/register.php
@@ -4,4 +4,7 @@ require 'db.php';
 // Check if the form was submitted via POST
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    // Sanitize the username input to prevent XSS attacks
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+
     
