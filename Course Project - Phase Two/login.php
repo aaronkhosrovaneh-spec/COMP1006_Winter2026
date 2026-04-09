1195e491101817e9314f18b70e51bc33c51c0456
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-09 14:48:12 -0400

    Prepared SQL query to select user with given username

--- Course Project - Phase Two/login.php
+++ Course Project - Phase Two/login.php
@@ -6,3 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Get the username and password from the POST request
     $username = $_POST['username'];
     $password = $_POST['password'];
+
+    // Prepare SQL query to select the user with the given username
+    $sql = "SELECT * FROM users WHERE username = :username";
+    $stmt = $db->prepare($sql);
