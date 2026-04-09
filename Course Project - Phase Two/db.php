524c269674c1d0b87189e8096f0be53a08231c37
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-09 14:21:06 -0400

    Set the error mode to exception

--- Course Project - Phase Two/db.php
+++ Course Project - Phase Two/db.php
@@ -6,3 +6,6 @@ $password = 'your_password';
 try {
     // Create a new PDO (PHP Data Objects) connection
     $db = new PDO($dsn, $user, $password);
+    // Set the error mode to exception
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
