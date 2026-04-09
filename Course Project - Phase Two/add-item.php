309c711310f509f0f93ba941b46158ef2cbba393
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-09 14:57:32 -0400

    Redirected to login page

--- Course Project - Phase Two/add-item.php
+++ Course Project - Phase Two/add-item.php
@@ -4,3 +4,7 @@ require 'db.php';
 
 // Authorization Check: Restrict page access to logged-in users
 if (!isset($_SESSION['userId'])) {
+    // If the user is not logged in, redirect to login page
+    header("Location: login.php");
+    exit();
+}
