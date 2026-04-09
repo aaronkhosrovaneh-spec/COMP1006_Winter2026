be071de926ba27bcf2b3ad04df73c653fb7ea448
aaronkhosrovaneh-spec <aaronkhosrovaneh@gmail.com>
2026-04-09 14:20:26 -0400

    Created new PDO connection

--- Course Project - Phase Two/db.php
+++ Course Project - Phase Two/db.php
@@ -2,3 +2,7 @@
 $dsn = 'mysql:host=localhost;dbname=your_db_name';
 $user = 'your_username';
 $password = 'your_password';
+
+try {
+    // Create a new PDO (PHP Data Objects) connection
+    $db = new PDO($dsn, $user, $password);
